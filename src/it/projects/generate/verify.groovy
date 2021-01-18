853ca2517598ea30539e238875437e645e3e23e5
Gerard Dethier <g.dethier@gmail.com>
2021-01-18 12:12:33 +0100

    feat: align with latest PC features.

--- src/it/projects/generate/verify.groovy
+++ src/it/projects/generate/verify.groovy
@@ -3,5 +3,5 @@ def testProjectDirectory = new File("target/test-classes/projects/generate/proje
 def processClassFile = new File(testProjectDirectory, "src/main/java/mymodule/process/MyProcess.java")
 assert processClassFile.exists() : processClassFile.getAbsolutePath() + " does not exist"
 
-def aggregateRootClassFile = new File(testProjectDirectory, "src/main/java/mymodule/model/myaggregate/MyAggregateRoot.java")
-assert aggregateRootClassFile.exists() : aggregateRootClassFile.getAbsolutePath() + " does not exist"
+def aggregateClassFile = new File(testProjectDirectory, "src/main/java/mymodule/model/myaggregate/MyAggregate.java")
+assert aggregateClassFile.exists() : aggregateClassFile.getAbsolutePath() + " does not exist"
